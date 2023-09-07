5e366b6209c9e7958fc3c8a5dbfb49b7879a6cdf
chye0001 <chye0001@stud.kea.dk>
2023-09-07 15:07:11 +0200

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World\n"+"Hello dat23D");
+        System.out.println("Dette er årstal: 23");
+        LocalDate Today = LocalDate.now();
+
+        System.out.println("I dags dato: "+ Today);
+        System.out.println("Datoen: "+ LocalDate.now());
+
+        LocalDateTime TodayTime = LocalDateTime.now();
+
+        System.out.println(LocalDateTime.now());
+        System.out.println(LocalDateTime.now());
+    }
+}
